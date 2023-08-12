0959d3ce714afd10c44b4110a9d8c0d72c1561c5
chintan <patelchintan843@gmail.com>
2023-08-12 23:09:52 +0530

    easy coding ninjas

---
+++ Coding Ninja 86 Contest/ConvertNumber.cpp
@@ -0,0 +1,70 @@
+// string ninjaAndStringConversion(int n, string &s)
+// {
+//     // Write your code here.
+//     for (int i = 0; i < s.size(); i++)
+//     {
+//         if (s[i] >= 65 && s[i] <= 90)
+//         {
+//             if (s[i] == 65)
+//             {
+//                 s[i] = 90;
+//             }
+//             else
+//             {
+//                 s[i] -= 1;
+//             }
+//         }
+//         else
+//         {
+//             if (s[i] >= 97 && s[i] <= 122)
+//             {
+//                 if (s[i] == 122)
+//                 {
+//                     s[i] = 97;
+//                 }
+//                 else
+//                 {
+//                     s[i] += 1;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s = "Aad";
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] >= 65 && s[i] <= 90)
+        {
+            if (s[i] == 65)
+            {
+                s[i] = 90;
+            }
+            else
+            {
+                s[i] -= 1;
+            }
+        }
+        else
+        {
+            if (s[i] >= 97 && s[i] <= 122)
+            {
+                if (s[i] == 122)
+                {
+                    s[i] = 97;
+                }
+                else
+                {
+                    s[i] += 1;
+                }
+            }
+        }
+    }
+    cout << s;
+    return 0;
+}
